2d638aa6982ca2463f3c382c63e9907f01bae5fa
Piotr Zawadzki <piotr.98zawadzki@gmail.com>
2021-11-17 21:24:44 +0100

    commented out all tests

---
+++ src/test/java/org/piterconsulting/service/AccountServiceTest.java
@@ -0,0 +1,22 @@
+package org.piterconsulting.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.piterconsulting.repository.AccountRepository;
+import org.piterconsulting.repository.entity.Account;
+import org.piterconsulting.repository.entity.Client;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountServiceTest {
+
+
+
+    @Test
+    void transfer() {
+    }
+}
